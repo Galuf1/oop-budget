0e5056dd6a3c7f3db399ea1d980f06d2383132b2
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-06-06 22:19:08 +0200

    class done, runner incomplete

---
+++ User.py
@@ -0,0 +1,51 @@
+import csv
+from tempfile import gettempprefix
+from unicodedata import category
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self._income = 0
+        self._categories = {}
+
+
+    def __str__(self):
+
+        return f"Your monthly budget is {self.budget}"
+
+    @property
+    def income(self):
+        return self._income
+
+    @income.setter
+    def income(self, new_income):
+        if new_income > self._income:
+            self._income = new_income
+        else: 
+            print("can't do")
+
+    @property
+    def categories(self):
+        return self._categories
+
+    @categories.setter
+    def categories(self, cat):
+        self._categories[cat[0]] = cat[1]
+        self._income -= cat[1]
+
+    def monthly_costs(self):
+        sum = 0
+        for i in self.categories:
+            sum += self.categories.get(i,0)
+        return sum
+    
+    def add_category(self, new_cat):
+        self._categories[new_cat] = 0
+    
+    def get_all_percentages(self):
+        for i,j in self._categories.items():
+            percent = str(round((j/self._income),2))
+            print(f"{i} {percent}%")
+
+
+    
--- runner.py
+++ runner.py
@@ -1,1 +1,12 @@
-# After you write all your classes, use this file to call them all together and run your program
+from importlib.resources import path
+import csv
+from User import User
+
+path = "data/costs.csv"
+cat=[]
+with open(path,'w') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        cat.append(cat(**dict(row)))
+
+print(cat)
